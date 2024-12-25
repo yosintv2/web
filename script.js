62773ea8f83a6478d005aadf8e5ecc41e1ee1f08
Anynomous <156112254+yosintv2@users.noreply.github.com>
2024-12-25 23:38:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
         // List of leagues and their corresponding JSON files
         const leagues = [
            { id: 'yosintv-cricket', file: 'cricket.json', title: 'Cricket' },
-           { id: 'yosintv-npl', file: 'npl.json', title: 'NPL T20' },
+           { id: 'yosintv-cleague', file: 'cleague.json', title: 'Cricket Leagues' },
+            { id: 'yosintv-npl', file: 'npl.json', title: 'NPL T20' },
            { id: 'yosintv-ucl', file: 'ucl.json', title: 'Champions League' },
             { id: 'yosintv-football', file: 'football.json', title: 'Football' },
             { id: 'yosintv-epl', file: 'epl.json', title: 'EPL' },
