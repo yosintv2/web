fb57916c5b9afba76ad46a2e6b0424c4d64ae39c
Anynomous <156112254+yosintv2@users.noreply.github.com>
2025-09-04 15:14:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-  <script>
 const leagues = [
   { id: 'yosintv-fight', file: 'https://yosintv11.pages.dev/fight.json', title: 'UFC/MMA/Boxing' },
   { id: 'yosintv-cricket', file: 'https://yosintv11.pages.dev/cricket.json', title: 'Top Cricket' },
